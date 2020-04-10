eff45b417a35bbc07a47f7288ec3b6ba276e019c
Trust <fryguy503@gmail.com>
2020-04-10 06:04:40 +0000

    [Bug] NPCs will now only proc on hit
    Code by: rspurrell (Commit ee3d22437dfa3f098b90326b4c9c7c8836d3ecde public p2002eq/game_server)

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -2058,8 +2058,9 @@ bool NPC::Attack(Mob* other, int Hand, bool bRiposte, bool IsStrikethrough, bool
 		if (other->IsClient() && IsPet() && GetOwner()->IsClient()) {
 			//pets do half damage to clients in pvp
 			my_hit.damage_done /= 2;
-			if (my_hit.damage_done < 1)
-				my_hit.damage_done = 1;
+			if (my_hit.damage_done < 1) {
+                my_hit.damage_done = 1;
+            }
 		}
 	}
 	else {
@@ -2069,38 +2070,41 @@ bool NPC::Attack(Mob* other, int Hand, bool bRiposte, bool IsStrikethrough, bool
 	if (GetHP() > 0 && !other->HasDied()) {
 		other->Damage(this, my_hit.damage_done, SPELL_UNKNOWN, my_hit.skill, true, -1, false, m_specialattacks); // Not avoidable client already had thier chance to Avoid
 	}
-	else
-		return false;
+	else {
+        return false;
+    }
 
-	if (HasDied()) //killed by damage shield ect
-		return false;
+	if (HasDied()) { //killed by damage shield ect
+        return false;
+    }
 
 	MeleeLifeTap(my_hit.damage_done);
 
 	CommonBreakInvisibleFromCombat();
 
 	//I doubt this works...
-	if (!GetTarget())
-		return true; //We killed them
+	if (!GetTarget()) {
+        return true; //We killed them
+    }
 
-	if (!bRiposte && !other->HasDied()) {
-		TryWeaponProc(nullptr, weapon, other, Hand);	//no weapon
+    bool hasHit = my_hit.damage_done > 0;
+    if (hasHit && !bRiposte && !other->HasDied()) {
+        TryWeaponProc(nullptr, weapon, other, Hand);
 
-		if (!other->HasDied())
-			TrySpellProc(nullptr, weapon, other, Hand);
+        if (!other->HasDied()) {
+            TrySpellProc(nullptr, weapon, other, Hand);
+        }
 
-		if (my_hit.damage_done > 0 && HasSkillProcSuccess() && !other->HasDied())
-			TrySkillProc(other, my_hit.skill, 0, true, Hand);
-	}
+        if (HasSkillProcSuccess() && !other->HasDied()) {
+            TrySkillProc(other, my_hit.skill, 0, true, Hand);
+        }
+    }
 
-	if (GetHP() > 0 && !other->HasDied())
-		TriggerDefensiveProcs(other, Hand, true, my_hit.damage_done);
+	if (GetHP() > 0 && !other->HasDied()) {
+        TriggerDefensiveProcs(other, Hand, true, my_hit.damage_done);
+    }
 
-	if (my_hit.damage_done > 0)
-		return true;
-
-	else
-		return false;
+    return hasHit;
 }
 
 void NPC::Damage(Mob* other, int32 damage, uint16 spell_id, EQEmu::skills::SkillType attack_skill, bool avoidable, int8 buffslot, bool iBuffTic, eSpecialAttacks special) {
