f4060227861d3fb4fd0d627730b6a870d8841a84
Trust <fryguy503@gmail.com>
2020-06-28 18:30:36 -0400

    [HT] Lets try this?

--- zone/effects.cpp
+++ zone/effects.cpp
@@ -91,6 +91,16 @@ int32 Mob::GetActSpellDamage(uint16 spell_id, int32 value, Mob* target) {
 
 			value = value_BaseEffect*ratio/100;
 
+			// Need to scale HT damage differently after level 40! It no longer scales by the constant value in the spell file. It scales differently, instead of 10 more damage per level, it does 30 more damage per level. So we multiply the level minus 40 times 20 if they are over level 40.
+			if ((spell_id == SPELL_HARM_TOUCH || spell_id == SPELL_HARM_TOUCH2 || spell_id == SPELL_IMP_HARM_TOUCH ) && GetLevel() > 40) {
+				value -= (GetLevel() - 40) * 20;
+			}
+
+			//This adds the extra damage from the AA Unholy Touch, 450 per level to the AA Improved Harm TOuch.
+			if (spell_id == SPELL_IMP_HARM_TOUCH && IsClient()) {//Improved Harm Touch
+				value -= GetAA(aaUnholyTouch) * 450; //Unholy Touch
+			}
+			
 			value += value_BaseEffect*GetFocusEffect(focusImprovedDamage, spell_id)/100;
 			value += value_BaseEffect*GetFocusEffect(focusImprovedDamage2, spell_id)/100;
 
@@ -119,15 +129,6 @@ int32 Mob::GetActSpellDamage(uint16 spell_id, int32 value, Mob* target) {
 			if (spell_id == 2751) {
 				if (value < -MBCap)
 					value = -MBCap;
-			}
-			// Need to scale HT damage differently after level 40! It no longer scales by the constant value in the spell file. It scales differently, instead of 10 more damage per level, it does 30 more damage per level. So we multiply the level minus 40 times 20 if they are over level 40.
-			if ((spell_id == SPELL_HARM_TOUCH || spell_id == SPELL_HARM_TOUCH2 || spell_id == SPELL_IMP_HARM_TOUCH ) && GetLevel() > 40) {
-				value -= (GetLevel() - 40) * 20;
-			}
-
-			//This adds the extra damage from the AA Unholy Touch, 450 per level to the AA Improved Harm TOuch.
-			if (spell_id == SPELL_IMP_HARM_TOUCH && IsClient()) {//Improved Harm Touch
-				value -= GetAA(aaUnholyTouch) * 450; //Unholy Touch
 			}
 			
 			entity_list.MessageClose_StringID(this, true, 100, Chat::SpellCrit,
