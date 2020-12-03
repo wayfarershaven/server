257d5dd39c5a6bccb415fb293b645ee1f601886d
Trust <fryguy503@gmail.com>
2020-12-03 11:54:37 -0500

    [REBASE] AoE and Xtarget fix

--- zone/groups.cpp
+++ zone/groups.cpp
@@ -352,9 +352,6 @@ bool Group::AddMember(Mob* newmember, const char *NewMemberName, uint32 Characte
 		database.SetGroupID(NewMemberName, GetID(), CharacterID, ismerc);
 	}
 
-	if (newmember && newmember->IsClient())
-		newmember->CastToClient()->JoinGroupXTargets(this);
-
 	safe_delete(outapp);
 
 #ifdef BOTS
