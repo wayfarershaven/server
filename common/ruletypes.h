6d4c4e0d57a45108f8e22e8de698962c84f10ef1
Trust <fryguy503@gmail.com>
2020-12-03 09:57:03 -0500

    [REBASE] Rogue Backstab Haste Patch
    Implemented accuracy adjustment to Rogue Backstab to compensate for excessive impact of Haste on Backstab Cooldown
    The maximum reduction to backstab should be 2 seconds (1 s at 40% worn, 1 s at 100% spell), bringing it from a 10 second CD to an 8 second CD.  Due to how the code works, it seems that the Backstab cooldown (there are two) that ultimately controls Backstab maybe client side and impacted entirely by spell haste (but not worn haste).  To prevent nerfing every other class during adjusting Rogues, accuracy of backstab was adjusted based on haste to bring the total backstab DPS in line with where it would be if haste was working correctly.

--- common/ruletypes.h
+++ common/ruletypes.h
@@ -475,6 +475,7 @@ RULE_INT(Combat, SneakPullAssistRange, 400, "Modified range of assist for sneak 
 RULE_INT(Combat, RampageDistance, 100, "Distance a mob can Rampage")
 RULE_INT(Combat, TripleAttackChanceWarrior, 125, "Innate Chance for Warrior to Triple Attack after a Double Attack (125 = 12.5%). DEFAULT: 125")
 RULE_INT(Combat, TripleAttackChanceMonk, 200, "Innate Chance for Monk to Triple Attack after a Double Attack (200 = 20%). DEFAULT: 200")
+RULE_BOOL(Combat, RogueBackstabHasteCorrection, true, "Toggle to enable correction for Haste impacting Backstab DPS too much.  DEFAULT: true")
 RULE_REAL(Combat, SlayDamageAdjustment, 1.0, "Slay Damage Adjustment")
 RULE_CATEGORY_END()
 
