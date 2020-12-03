641a59b5b77d5889bbf58c14d2d614533087cf3a
Trust <fryguy503@gmail.com>
2020-12-03 15:14:31 -0500

    [REBASE] Pet Hate fixes
    pets shouldn't get hate from mobs healing each other

--- zone/aggro.cpp
+++ zone/aggro.cpp
@@ -1187,11 +1187,14 @@ int32 Mob::CheckHealAggroAmount(uint16 spell_id, Mob *target, uint32 heal_possib
 			break;
 		}
 	}
-	if (GetOwner() && IsPet())
-		AggroAmount = AggroAmount * RuleI(Aggro, PetSpellAggroMod) / 100;
 
-	if (!ignore_default_buff && IsBuffSpell(spell_id) && IsBeneficialSpell(spell_id))
+	if (!ignore_default_buff && IsBuffSpell(spell_id) && IsBeneficialSpell(spell_id)) {
 		AggroAmount = IsBardSong(spell_id) ? 2 : 9;
+	}
+
+	if (GetOwner() && IsPet()) {
+		AggroAmount = AggroAmount * RuleI(Aggro, PetSpellAggroMod) / 100;
+	}
 
 	if (AggroAmount > 0) {
 		int HateMod = RuleI(Aggro, SpellAggroMod);
