d5107d88998c7e2210bd6e1beacd80b9eb556040
Trust <fryguy503@gmail.com>
2020-12-04 14:22:39 -0500

    [REBASE] Fix underwater boats for Maidens_Voyage, Icebreaker, Bloated_Belly, Sea_King, SirensBane and small boats.

--- zone/entity.cpp
+++ zone/entity.cpp
@@ -690,6 +690,10 @@ void EntityList::AddNPC(NPC *npc, bool SendSpawnPacket, bool dontqueue)
 
 	parse->EventNPC(EVENT_SPAWN, npc, nullptr, "", 0);
 
+	if(npc->GetRace() != 72 && npc->GetRace() != 73 && npc->GetRace() != 141 ) {
+		npc->FixZ(1);
+	}
+
 	uint16 emoteid = npc->GetEmoteID();
 	if (emoteid != 0)
 		npc->DoNPCEmote(ONSPAWN, emoteid);
