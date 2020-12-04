282a9afbc276dcd9b5293310f726c4822ee0f159
Trust <fryguy503@gmail.com>
2020-12-04 00:33:09 -0500

    [REBASE] force fd break on spell cast

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -4054,6 +4054,11 @@ void Client::Handle_OP_CancelTrade(const EQApplicationPacket *app)
 
 void Client::Handle_OP_CastSpell(const EQApplicationPacket *app)
 {
+	//break feign if casting
+	if (feigned) {
+		SetFeigned(false);
+	}
+
 	using EQ::spells::CastingSlot;
 	if (app->size != sizeof(CastSpell_Struct)) {
 		std::cout << "Wrong size: OP_CastSpell, size=" << app->size << ", expected " << sizeof(CastSpell_Struct) << std::endl;
