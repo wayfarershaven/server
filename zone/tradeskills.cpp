9d2946040b48cf5d5e6c415b11a065f7365db4c2
Trust <fryguy503@gmail.com>
2020-12-04 17:27:42 -0500

    [REBASE] Clamp Tradeskill Skills to 252 max

--- zone/tradeskills.cpp
+++ zone/tradeskills.cpp
@@ -923,10 +923,16 @@ void Client::SendTradeskillDetails(uint32 recipe_id) {
 
 //returns true on success
 bool Client::TradeskillExecute(DBTradeskillRecipe_Struct *spec) {
-	if(spec == nullptr)
-		return(false);
+	if(spec == nullptr) {
+		return (false);
+	}
 
 	uint16 user_skill = GetSkill(spec->tradeskill);
+
+	if (user_skill > 252) {
+		user_skill = 252;
+	}
+
 	float chance = 0.0;
 	float skillup_modifier = 0.0;
 	int16 thirdstat = 0;
