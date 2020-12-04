aac85496a7a10956b8bfb5070552002f0fc3b7cd
Trust <fryguy503@gmail.com>
2020-12-04 17:51:15 -0500

    [REBASE] Disable Selling Bags in Trader Mode.

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -14431,6 +14431,12 @@ void Client::Handle_OP_Trader(const EQApplicationPacket *app)
 					TradeItemsValid = false;
 					break;
 				}
+
+				if(Item->IsClassBag()) {
+					Message(Chat::Red, "Not allowed to sell bags, please convert to a token. Unable to start trader mode.");
+					TradeItemsValid = false;
+					break;
+				}
 			}
 
 			if (!TradeItemsValid) {
