3c41e45a72b9642df953b6fcf9c5705bac7d208b
fryguy <fryguy503@gmail.com>
2021-06-05 20:51:43 -0400

    [Bug] Limit MeleeMitigationEffect to defender if only client.

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -4701,8 +4701,9 @@ void Mob::DoRiposte(Mob *defender)
     if (DoubleRipChance && zone->random.Roll(DoubleRipChance)) {
         LogCombat("Preforming a double riposted from SE_GiveDoubleRiposte base1 == 0 ([{}] percent chance)", DoubleRipChance);
         defender->Attack(this, EQ::invslot::slotPrimary, true);
-        if (HasDied())
+        if (HasDied()) {
             return;
+        }
     }
 
     // Double Riposte effect, allows for a chance to do RIPOSTE with a skill specific special attack (ie Return Kick).
@@ -4713,10 +4714,11 @@ void Mob::DoRiposte(Mob *defender)
     if (DoubleRipChance && zone->random.Roll(DoubleRipChance)) {
         LogCombat("Preforming a return SPECIAL ATTACK ([{}] percent chance)", DoubleRipChance);
 
-        if (defender->GetClass() == MONK)
+        if (defender->GetClass() == MONK) {
             defender->MonkSpecialAttack(this, defender->aabonuses.GiveDoubleRiposte[2]);
-        else if (defender->IsClient()) // so yeah, even if you don't have the skill you can still do the attack :P (and we don't crash anymore)
+        } else if (defender->IsClient()) { // so yeah, even if you don't have the skill you can still do the attack :P (and we don't crash anymore)
             defender->CastToClient()->DoClassAttacks(this, defender->aabonuses.GiveDoubleRiposte[2], true);
+        }
     }
 }
 
@@ -4725,19 +4727,21 @@ void Mob::ApplyMeleeDamageMods(uint16 skill, int &damage, Mob *defender, ExtraAt
     int dmgbonusmod = 0;
 
     dmgbonusmod += GetMeleeDamageMod_SE(skill);
-    if (opts)
+    if (opts) {
         dmgbonusmod += opts->melee_damage_bonus_flat;
-
-    if (defender) {
-        if (defender->IsClient() && defender->GetClass() == WARRIOR) {
-            dmgbonusmod -= 5;
-        }
-        // 168 defensive
-        dmgbonusmod += (defender->spellbonuses.MeleeMitigationEffect +
-                        defender->itembonuses.MeleeMitigationEffect +
-                        defender->aabonuses.MeleeMitigationEffect);
     }
 
+    if (defender) {
+		// Innate Warrior Mitigation, implemented 2004
+		if (defender->IsClient() && defender->GetClass() == WARRIOR) {
+			dmgbonusmod -= 5;
+		}
+
+		if (defender->IsClient()) {
+			dmgbonusmod -= (defender->spellbonuses.MeleeMitigationEffect + itembonuses.MeleeMitigationEffect +
+							aabonuses.MeleeMitigationEffect);
+		}
+	}
     damage += damage * dmgbonusmod / 100;
 }
 
@@ -4747,8 +4751,9 @@ bool Mob::HasDied() {
 
     hp_below = (GetDelayDeath() * -1);
 
-    if ((GetHP()) <= (hp_below))
+    if ((GetHP()) <= (hp_below)) {
         Result = true;
+    }
 
     return Result;
 }
