30196d6d198fef0b387a85eafcc0da1dfa97a973
fryguy <fryguy503@gmail.com>
2021-06-14 22:12:33 -0400

    Revert "[Fix] Fix Spell Cast Time reduction issues"
    This reverts commit 8105da1f72403fd882009163aa2d7aee47895384.

--- zone/mob.cpp
+++ zone/mob.cpp
@@ -3368,16 +3368,25 @@ uint32 Mob::GetLevelHP(uint8 tlevel)
 }
 
 int32 Mob::GetActSpellCasttime(uint16 spell_id, int32 casttime) {
-	int32 cast_reducer = GetFocusEffect(focusSpellHaste, spell_id);
 
-	if (level > 50 && casttime >= 3000 && !spells[spell_id].goodEffect &&
-		(GetClass() == RANGER || GetClass() == SHADOWKNIGHT || GetClass() == PALADIN || GetClass() == BEASTLORD)) {
-		int level_mod = std::min(15, GetLevel() - 50);
-		cast_reducer += level_mod * 3;
+	int32 cast_reducer = 0;
+	cast_reducer += GetFocusEffect(focusSpellHaste, spell_id);
+
+	if (level >= 60 && casttime > 1000)
+	{
+		casttime = casttime / 2;
+		if (casttime < 1000)
+			casttime = 1000;
+	} else if (level >= 50 && casttime > 1000) {
+		int32 cast_deduction = (casttime*(level - 49))/5;
+		if (cast_deduction > casttime/2)
+			casttime /= 2;
+		else
+			casttime -= cast_deduction;
 	}
 
-	casttime = casttime * (100 - cast_reducer) / 100;
-	return std::max(casttime, casttime / 2);
+	casttime = (casttime*(100 - cast_reducer)/100);
+	return casttime;
 }
 
 void Mob::ExecWeaponProc(const EQEmu::ItemInstance *inst, uint16 spell_id, Mob *on, int level_override) {
