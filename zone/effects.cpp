62ab891416b6954ac42085c65a11a254c0ba42d1
Trust <fryguy503@gmail.com>
2021-11-30 23:56:39 -0500

    [Gates] Adjust focus to only effect non discs except warcries.

--- zone/effects.cpp
+++ zone/effects.cpp
@@ -443,6 +443,10 @@ int32 Client::GetActSpellCost(uint16 spell_id, int32 cost)
 
 int32 Mob::GetActSpellDuration(uint16 spell_id, int32 duration)
 {
+	if (IsDiscipline(spell_id) && (!spell_id == 5027 || !spell_id == 5028 || !spell_id == 5029 || !spell_id == 5030 || !spell_id == 5031 || !spell_id == 5032 || !spell_id == 8476)) { // focuses don't affect discipline duration (Except War Cries)
+		return duration;
+	}
+
 	int increase = 100;
 	increase += GetFocusEffect(focusSpellDuration, spell_id);
 	int tic_inc = 0;
