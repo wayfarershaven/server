6a7a79f628e700b744c8fe1f1a964f5a7296b59b
Trust <fryguy503@gmail.com>
2021-12-01 23:43:00 -0500

    [Bug Fix] - Autofire attacking yourself Fix being able to attack yourself with autofire if Combat:MinRangedAttackDist == 0

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -3369,6 +3369,13 @@ void Client::Handle_OP_AutoFire(const EQApplicationPacket *app)
 		DumpPacket(app);
 		return;
 	}
+
+	if (GetTarget() == this) {
+		MessageString(Chat::TooFarAway, TRY_ATTACKING_SOMEONE);
+		auto_fire = false;
+		return;
+	}
+
 	bool *af = (bool*)app->pBuffer;
 	auto_fire = *af;
 	auto_attack = false;
