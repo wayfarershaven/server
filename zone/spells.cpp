b9b6b6e0c7495ab218abbcfb3fc18fb4568de3af
Trust <fryguy503@gmail.com>
2022-01-20 17:02:28 -0500

    [Spells] NPC spell push should work on rooted mobs. * Update spells.cpp
    * [Spells] NPC spell push should work on rooted mobs.
    
    don't push perma or psuedorooted mobs
    
    EQEmu PR 1941

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -4172,7 +4172,7 @@ bool Mob::SpellOnTarget(uint16 spell_id, Mob *spelltar, bool reflect, bool use_r
 				spelltar->CastToClient()->cheat_manager.SetExemptStatus(KnockBack, true);
 			}
 		}
-		else if (RuleB(Spells, NPCSpellPush) && !spelltar->IsRooted() && spelltar->ForcedMovement == 0) {
+		else if (RuleB(Spells, NPCSpellPush) && !permarooted && !IsPseudoRooted() && spelltar->ForcedMovement == 0) {
 			if ((!resisted) && (!spelltar->IsImmuneToSpell(spell_id, this))) {
 				spelltar->m_Delta.x += action->force * g_Math.FastSin(action->hit_heading);
 				spelltar->m_Delta.y += action->force * g_Math.FastCos(action->hit_heading);
