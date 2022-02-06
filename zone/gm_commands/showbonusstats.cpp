d261a5241fea77772f6dd1ccaa3c227c84724481
Trust <fryguy503@gmail.com>
2022-02-06 00:51:56 -0500

    [REBASE] More Debuging

--- zone/gm_commands/showbonusstats.cpp
+++ zone/gm_commands/showbonusstats.cpp
@@ -42,14 +42,18 @@ void command_showbonusstats(Client *c, const Seperator *sep)
 				c->GetTarget()->GetSpellBonuses().FlurryChance,
 				c->GetTarget()->GetSpellBonuses().HitChance / 15
 			);
+			c->Message(Chat::White, " Crit: %i%%", c->GetTarget()->GetSpellBonuses().CriticalHitChance);
+			if (c->GetTarget()->GetClass() == PALADIN) {
+				c->Message(Chat::White, "Slay Rate: %i%%m Slay Damage Mod: %i%%", c->GetTarget()->GetSpellBonuses().SlayUndead[0], c->GetTarget()->GetSpellBonuses().SlayUndead[1]);
+			}
 		}
 		if (bAll || (strcasecmp(sep->arg[1], "aa") == 0)) {
-			c->Message(0, " Ambidexterity: %i%% ", c->GetTarget()->GetAABonuses().Ambidexterity);
+			c->Message(Chat::White, " Ambidexterity: %i%% ", c->GetTarget()->GetAABonuses().Ambidexterity);
 			if (c->GetTarget()->IsPet()) {
-				c->Message(0, " PetFlurry: %i%% ", c->GetTarget()->GetAABonuses().PetFlurry);
-				c->Message(0, " PC_Pet_Flurry: %i%% ", c->GetTarget()->GetAABonuses().PC_Pet_Flurry);
-				c->Message(0, " PC_Pet_Rampage: %i%% ", c->GetTarget()->GetAABonuses().PC_Pet_Rampage);
-				c->Message(0, " PetCriticalHit: %i%% ", c->GetTarget()->GetAABonuses().PetCriticalHit);
+				c->Message(Chat::White, " PetFlurry: %i%% ", c->GetTarget()->GetAABonuses().PetFlurry);
+				c->Message(Chat::White, " PC_Pet_Flurry: %i%% ", c->GetTarget()->GetAABonuses().PC_Pet_Flurry);
+				c->Message(Chat::White, " PC_Pet_Rampage: %i%% ", c->GetTarget()->GetAABonuses().PC_Pet_Rampage);
+				c->Message(Chat::White, " PetCriticalHit: %i%% ", c->GetTarget()->GetAABonuses().PetCriticalHit);
 			}
 		}
 		c->Message(Chat::White, "  Effective Casting Level: %i", c->GetTarget()->GetCasterLevel(0));
