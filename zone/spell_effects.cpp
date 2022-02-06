6ff916917f0e4c4799c86f5cfd30c0bf3e918dcc
Trust <fryguy503@gmail.com>
2022-02-05 23:39:22 -0500

    [REBASE] multiplication is wrong here, changing to addition

--- zone/spell_effects.cpp
+++ zone/spell_effects.cpp
@@ -8522,7 +8522,7 @@ void Client::BreakFeignDeathWhenCastOn(bool IsResisted) {
 		int chance = spellbonuses.FeignedCastOnChance + itembonuses.FeignedCastOnChance + aabonuses.FeignedCastOnChance;
 
 		if (IsResisted) {
-			chance *= RuleR(Spells, BreakFeignDeathWhenCastOn);
+			chance += RuleR(Spells, BreakFeignDeathWhenCastOn);
 		}
 
 		if(chance && (zone->random.Roll(chance))){
