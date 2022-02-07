73f7c2e6cfe95ef3ca007c3ef4512fab97685690
Trust <fryguy503@gmail.com>
2022-02-06 23:25:07 -0500

    [REBASE] AOE Fixes

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -199,16 +199,15 @@ bool Mob::CastSpell(uint16 spell_id, uint16 target_id, CastingSlot slot,
 		BuffFadeByEffect(SE_NegateIfCombat);
 	}
 
-	// check line of sight to target if it's a detrimental spell
-	if (!spells[spell_id].npc_no_los && GetTarget() && IsDetrimentalSpell(spell_id) && !CheckLosFN(GetTarget()) && !IsHarmonySpell(spell_id) && spells[spell_id].target_type != ST_TargetOptional && !IsBindSightSpell(spell_id))
-	{
-		LogSpells("Spell [{}]: cannot see target [{}]", spell_id, GetTarget()->GetName());
-		MessageString(Chat::Red, CANT_SEE_TARGET);
-		if (IsClient()) {
-			CastToClient()->SendSpellBarEnable(spell_id);
-		}
-		return(false);
-	}
+    // check line of sight to target if it's a detrimental spell
+    if (spells[spell_id].target_type != ST_AECaster && !spells[spell_id].npc_no_los && GetTarget() && IsDetrimentalSpell(spell_id) && !CheckLosFN(GetTarget()) && !IsHarmonySpell(spell_id) && spells[spell_id].target_type != ST_TargetOptional && !IsBindSightSpell(spell_id))
+    {
+        Log(Logs::Detail, Logs::Spells, "Spell %d: cannot see target %s", spell_id, GetTarget()->GetName());
+        MessageString(13, CANT_SEE_TARGET);
+        if (IsClient())
+            CastToClient()->SendSpellBarEnable(spell_id);
+        return(false);
+    }
 
 	// check to see if target is a caster mob before performing a mana tap
 	if(GetTarget() && IsManaTapSpell(spell_id)) {
@@ -2486,9 +2485,9 @@ bool Mob::SpellFinished(uint16 spell_id, Mob *spell_target, CastingSlot slot, ui
 			break;
 		}
 
-		case AECaster:
-		case AETarget:
-		{
+        case AECaster:
+        case AETarget:
+        {
 #ifdef BOTS
 			if(IsBot()) {
 				bool StopLogic = false;
@@ -2506,22 +2505,7 @@ bool Mob::SpellFinished(uint16 spell_id, Mob *spell_target, CastingSlot slot, ui
 				// special ae duration spell
 				ae_center->CastToBeacon()->AELocationSpell(this, spell_id, resist_adjust);
 			} else {
-				// unsure if we actually need this? Need to find some spell examples
-				// regular PB AE or targeted AE spell - spell_target is null if PB
-				if(spell_target)	// this must be an AETarget spell
-				{
-					bool cast_on_target = true;
-					if (spells[spell_id].target_type == ST_TargetAENoPlayersPets && spell_target->IsPetOwnerClient())
-						cast_on_target = false;
-					if (spells[spell_id].target_type == ST_AreaClientOnly && !spell_target->IsClient())
-						cast_on_target = false;
-					if (spells[spell_id].target_type == ST_AreaNPCOnly && !spell_target->IsNPC())
-						cast_on_target = false;
-
-					// affect the target too
-					if (cast_on_target)
-						SpellOnTarget(spell_id, spell_target, false, true, resist_adjust);
-				}
+ 				// unsure if we actually need this? Need to find some spell examples
 				if(ae_center && ae_center == this && IsBeneficialSpell(spell_id))
 					SpellOnTarget(spell_id, this);
 
