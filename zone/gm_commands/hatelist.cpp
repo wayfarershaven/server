6555f04277af66fba343061baa64c425e0cd03bc
Trust <fryguy503@gmail.com>
2022-02-07 21:32:10 -0500

    {EQEmu PR 1977} [Commands] Cleanup #name Command. Cleanup messages and logic.

--- zone/gm_commands/hatelist.cpp
+++ zone/gm_commands/hatelist.cpp
@@ -2,14 +2,12 @@
 
 void command_hatelist(Client *c, const Seperator *sep)
 {
-	Mob *target = c->GetTarget();
 	if (!c->GetTarget() || !c->GetTarget()->IsNPC()) {
 		c->Message(Chat::White, "You must target an NPC to use this command.");
 		return;
 	}
 
 	auto target = c->GetTarget();
+
 	target->PrintHateListToClient(c);
 }
-
-
