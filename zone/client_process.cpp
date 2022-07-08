b0f07361bec3e327009d302f62b3f0cde38c8250
Trust <fryguy503@gmail.com>
2022-07-08 15:38:05 -0400

    Revert "Revert "{EQEmu PR 2268} [Feature] Add Water Line of Sight Checks""
    This reverts commit ed8f3a9f5f02eac52c0c54f37c11a2a611b92bab.

--- zone/client_process.cpp
+++ zone/client_process.cpp
@@ -57,6 +57,8 @@
 #include "zonedb.h"
 #include "zone_store.h"
 
+#include "water_map.h"
+
 extern QueryServ* QServ;
 extern Zone* zone;
 extern volatile bool is_zone_loaded;
@@ -340,38 +342,39 @@ bool Client::Process() {
 					if (ranged_timer.Check(false)) {
 						if (GetTarget() && (GetTarget()->IsNPC() || GetTarget()->IsClient()) && IsAttackAllowed(GetTarget())) {
 							if (GetTarget()->InFrontMob(this, GetTarget()->GetX(), GetTarget()->GetY())) {
-								if (CheckLosFN(GetTarget())) {
+								if (CheckLosFN(GetTarget()) && CheckWaterAutoFireLoS(this, GetTarget())) {
 									//client has built in los check, but auto fire does not.. done last.
 									RangedAttack(GetTarget());
-									if (CheckDoubleRangedAttack())
+									if (CheckDoubleRangedAttack()) {
 										RangedAttack(GetTarget(), true);
-								}
-								else
+									}
+								} else {
 									ranged_timer.Start();
-							}
-							else
+								}
+							} else {
 								ranged_timer.Start();
-						}
-						else
+							}
+						} else {
 							ranged_timer.Start();
+						}
 					}
 				}
 				else if (ranged->GetItem() && (ranged->GetItem()->ItemType == EQ::item::ItemTypeLargeThrowing || ranged->GetItem()->ItemType == EQ::item::ItemTypeSmallThrowing)) {
 					if (ranged_timer.Check(false)) {
 						if (GetTarget() && (GetTarget()->IsNPC() || GetTarget()->IsClient()) && IsAttackAllowed(GetTarget())) {
 							if (GetTarget()->InFrontMob(this, GetTarget()->GetX(), GetTarget()->GetY())) {
-								if (CheckLosFN(GetTarget())) {
+								if (CheckLosFN(GetTarget()) && CheckWaterAutoFireLoS(this, GetTarget())) {
 									//client has built in los check, but auto fire does not.. done last.
 									ThrowingAttack(GetTarget());
-								}
-								else
+								} else {
 									ranged_timer.Start();
-							}
-							else
+								}
+							} else {
 								ranged_timer.Start();
-						}
-						else
+							}
+						} else {
 							ranged_timer.Start();
+						}
 					}
 				}
 			}
@@ -401,8 +404,7 @@ bool Client::Process() {
 					m_AutoAttackPosition.w = GetHeading();
 					los_status_facing = IsFacingMob(aa_los_them_mob);
 				}
-			}
-			else {
+			} else {
 				aa_los_them_mob            = auto_attack_target;
 				m_AutoAttackPosition       = GetPosition();
 				m_AutoAttackTargetLocation = glm::vec3(aa_los_them_mob->GetPosition());
@@ -412,14 +414,11 @@ bool Client::Process() {
 
 			if (!CombatRange(auto_attack_target)) {
 				MessageString(Chat::TooFarAway, TARGET_TOO_FAR);
-			}
-			else if (auto_attack_target == this) {
+			} else if (auto_attack_target == this) {
 				MessageString(Chat::TooFarAway, TRY_ATTACKING_SOMEONE);
-			}
-			else if (!los_status || !los_status_facing) {
+			} else if (!los_status || !los_status_facing) {
 				//you can't see your target
-			}
-			else if (auto_attack_target->GetHP() > -10 && IsAttackAllowed(auto_attack_target)) // -10 so we can watch people bleed in PvP
+			} else if (auto_attack_target->GetHP() > -10 && IsAttackAllowed(auto_attack_target)) // -10 so we can watch people bleed in PvP
 			{
 				EQ::ItemInstance *wpn = GetInv().GetItem(EQ::invslot::slotPrimary);
 				TryCombatProcs(wpn, auto_attack_target, EQ::invslot::slotPrimary);
@@ -459,12 +458,10 @@ bool Client::Process() {
 			// Don't attack yourself
 			else if (auto_attack_target == this) {
 				//MessageString(Chat::TooFarAway,TRY_ATTACKING_SOMEONE);
-			}
-			else if (!los_status || !los_status_facing)
+			} else if (!los_status || !los_status_facing)
 			{
 				//you can't see your target
-			}
-			else if (auto_attack_target->GetHP() > -10 && IsAttackAllowed(auto_attack_target)) {
+			} else if (auto_attack_target->GetHP() > -10 && IsAttackAllowed(auto_attack_target)) {
 				CheckIncreaseSkill(EQ::skills::SkillDualWield, auto_attack_target, -10);
 				if (CheckDualWield()) {
 					EQ::ItemInstance *wpn = GetInv().GetItem(EQ::invslot::slotSecondary);
@@ -2380,4 +2377,12 @@ void Client::SendGuildLFGuildStatus()
 
 	worldserver.SendPacket(pack);
 	safe_delete(pack);
+}
+
+bool Client::CheckWaterAutoFireLoS(Mob* los_attacker, Mob* los_target) // checks if both attacker and target are both in or out of the water
+{
+	if (!RuleB(Combat, WaterMatchRequiredForAutoFireLoS)) { // if rule is set to false, bypass check
+		return true;
+	}
+	return zone->watermap->InLiquid(los_attacker->GetPosition()) == zone->watermap->InLiquid(los_target->GetPosition());
 }
