10014f15a77162830c6e0c4894b53802553da499
Trust <fryguy503@gmail.com>
2022-12-04 19:19:03 -0500

    [REBASE] moved Disc message Minor adjustment

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -3628,10 +3628,10 @@ bool Mob::SpellOnTarget(
 
 	LogSpells("Casting spell [{}] on [{}] with effective caster level [{}]", spell_id, spelltar->GetName(), caster_level);
 
-	if (IsClient() && (IsDiscipline(spell_id) || IsDisciplineBuff(spell_id))) {
+	if (IsClient() && (IsDiscipline(spell_id) || spells[spell_id].is_discipline)) {
 		std::string msg = "%s";
 		msg += spells[spell_id].cast_on_other;
-		entity_list.MessageClose(this, true, 200, 0, msg.c_str(), this->GetCleanName());
+		entity_list.MessageClose(this, false, 200, 0, msg.c_str(), this->GetCleanName());
 	}
 
 	// Actual cast action - this causes the caster animation and the particles
