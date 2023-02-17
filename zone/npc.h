65f0938a5d7dab891f4db5027f31d44a88498a37
Trust <fryguy503@gmail.com>
2023-02-16 23:08:39 -0500

    {EQEmu PR 2830} [Crash] Fix IsUnderwaterOnly crash where npc data references can be stale
    ### What
    
    Fix IsUnderwaterOnly crash where npc data references can be stale
    
    The getter makes an assumption in the fallback which could also not be valid. This switches the access pattern to reference directly from the NPC instance member variable directly which will always be initialized to false
    
    Crash observed in v22.1.2 http://spire.akkadius.com/dev/release/22.1.2?id=265

--- zone/npc.h
+++ zone/npc.h
@@ -447,7 +447,7 @@ public:
 	/* Only allows players that killed corpse to loot */
 	const bool HasPrivateCorpse() const { return NPCTypedata_ours ? NPCTypedata_ours->private_corpse : NPCTypedata->private_corpse; }
 
-	virtual const bool IsUnderwaterOnly() const { return NPCTypedata_ours ? NPCTypedata_ours->underwater : NPCTypedata->underwater; }
+	virtual const bool IsUnderwaterOnly() const { return m_is_underwater_only; }
 	const char* GetRawNPCTypeName() const { return NPCTypedata_ours ? NPCTypedata_ours->name : NPCTypedata->name; }
 
 	void ChangeLastName(std::string last_name);
@@ -683,6 +683,8 @@ protected:
 	bool ldon_trap_detected;
 	QGlobalCache *qGlobals;
 	uint32 adventure_template_id;
+
+	bool m_is_underwater_only = false;
 
 	//mercenary stuff
 	std::list<MercType> mercTypeList;
