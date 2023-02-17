866c32f6bced71c6439433db6ddd6ba8f7827d03
Trust <fryguy503@gmail.com>
2023-02-17 08:17:39 -0500

    {EQEmu PR 2862} [Quest API] Export $object to EVENT_CLICK_OBJECT in Perl
    # Notes
    - Exports `$object` to `EVENT_CLICK_OBJECT` in Perl so you don't have to grab it from entity list.

--- zone/embparser.cpp
+++ zone/embparser.cpp
@@ -1764,6 +1764,9 @@ void PerlembParser::ExportEventVariables(
 		case EVENT_CLICK_OBJECT: {
 			ExportVar(package_name.c_str(), "objectid", data);
 			ExportVar(package_name.c_str(), "clicker_id", extradata);
+			if (extra_pointers && extra_pointers->size() == 1) {
+				ExportVar(package_name.c_str(), "object", "Object", std::any_cast<Object*>(extra_pointers->at(0)));
+			}
 			break;
 		}
 
