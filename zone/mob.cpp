wayfarershaven/server

commit 4a339d49df971217d82598bf4ad1dea35d3ea3fc

[Quest API] (Performance) Check event exists before export and execute EVENT_HP (#2904)
# Notes
- Optionally parse this event instead of always doing so.

zone/mob.cpp
@@ -1382,9 +1382,11 @@ void Mob::CreateHPPacket(EQApplicationPacket* app)
 	{
 		if (ds->hp < GetNextHPEvent())
 		{
-			std::string export_string = fmt::format("{}", GetNextHPEvent());
 			SetNextHPEvent(-1);
-			parse->EventNPC(EVENT_HP, CastToNPC(), nullptr, export_string, 0);
+
+			if (parse->HasQuestSub(GetNPCTypeID(), EVENT_HP)) {
+				parse->EventNPC(EVENT_HP, CastToNPC(), nullptr, std::to_string(GetNextHPEvent()), 0);
+			}
 		}
 	}
 
@@ -1392,9 +1394,11 @@ void Mob::CreateHPPacket(EQApplicationPacket* app)
 	{
 		if (ds->hp > GetNextIncHPEvent())
 		{
-			std::string export_string = fmt::format("{}", GetNextIncHPEvent());
 			SetNextIncHPEvent(-1);
-			parse->EventNPC(EVENT_HP, CastToNPC(), nullptr, export_string, 1);
+
+			if (parse->HasQuestSub(GetNPCTypeID(), EVENT_HP)) {
+				parse->EventNPC(EVENT_HP, CastToNPC(), nullptr, std::to_string(GetNextIncHPEvent()), 1);
+			}
 		}
 	}
 }

commit a2ec3a8ee4529f388c38b0ad6fca60ae53cb9eee

{EQEmu PR 2924} [Hotfix] Fix issue with EVENT_HP firing regression from EQEmu PR 2904
### What

Fix issue with EVENT_HP firing, regression from https://github.com/EQEmu/Server/pull/2904

zone/mob.cpp
@@ -1386,10 +1386,10 @@ void Mob::CreateHPPacket(EQApplicationPacket* app)
 	{
 		if (ds->hp < GetNextHPEvent())
 		{
-			SetNextHPEvent(-1);
-			if (parse->HasQuestSub(GetNPCTypeID(), EVENT_HP)) {
+			if (parse->HasQuestSub(CastToNPC()->GetNPCTypeID(), EVENT_HP)) {
 				parse->EventNPC(EVENT_HP, CastToNPC(), nullptr, std::to_string(GetNextHPEvent()), 0);
 			}
+			SetNextHPEvent(-1);
 		}
 	}
 
@@ -1397,10 +1397,10 @@ void Mob::CreateHPPacket(EQApplicationPacket* app)
 	{
 		if (ds->hp > GetNextIncHPEvent())
 		{
-			SetNextIncHPEvent(-1);
-			if (parse->HasQuestSub(GetNPCTypeID(), EVENT_HP)) {
+			if (parse->HasQuestSub(CastToNPC()->GetNPCTypeID(), EVENT_HP)) {
 				parse->EventNPC(EVENT_HP, CastToNPC(), nullptr, std::to_string(GetNextIncHPEvent()), 1);
 			}
+			SetNextIncHPEvent(-1);
 		}
 	}
 }