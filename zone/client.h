0deda2c7dc65f10acb5e0f6dcee1a0109f50e7ea
Trust <fryguy503@gmail.com>
2023-02-21 13:54:17 -0500

    {EQEmu PR 2977} [Tradeskills] Fix for Lore Conflict
    This fixes a regression issue where components of the combine could be lore, causing the combine to fail on the lore check if the components were returned on success, failure, or salvage.

--- zone/client.h
+++ zone/client.h
@@ -757,6 +757,7 @@ public:
 	void SendTradeskillDetails(uint32 recipe_id);
 	bool TradeskillExecute(DBTradeskillRecipe_Struct *spec);
 	void CheckIncreaseTradeskill(int16 bonusstat, int16 stat_modifier, float skillup_modifier, uint16 success_modifier, EQ::skills::SkillType tradeskill);
+	bool CheckTradeskillLoreConflict(DBTradeskillRecipe_Struct& spec, MySQLRequestResult& results);
 
 	void GMKill();
 	inline bool IsMedding() const {return medding;}
