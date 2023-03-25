ba0ac60dc861179db7952b374e9cf6e755327ac3
Trust <fryguy503@gmail.com>
2023-03-25 17:04:24 -0400

    {EQEmu PR 3074} [Cleanup] Remove _previousTarget from bot.h
    # Notes
    - This is unused.

--- zone/bot.cpp
+++ zone/bot.cpp
@@ -2119,7 +2119,6 @@ bool Bot::Process()
 
 		_botOwner = 0;
 		_botOwnerCharacterID = 0;
-		_previousTarget = 0;
 
 		return false;
 	}
@@ -2397,11 +2396,9 @@ bool Bot::CanDoSpecialAttack(Mob *other) {
 	return true;
 }
 
-void Bot::SetTarget(Mob* mob) {
-	if(mob != this) {
-		if(mob != GetTarget())
-			_previousTarget = GetTarget();
-
+void Bot::SetTarget(Mob *mob)
+{
+	if (mob != this) {
 		NPC::SetTarget(mob);
 	}
 }
@@ -3879,7 +3876,6 @@ void Bot::Depop() {
 
 	_botOwner = 0;
 	_botOwnerCharacterID = 0;
-	_previousTarget = 0;
 	NPC::Depop(false);
 }
 
