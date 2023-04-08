3f83901887d7f6c4e8d494e0971eeb8de203b9cb
Trust <fryguy503@gmail.com>
2023-04-07 23:58:15 -0400

    {EQEmu PR 3240} [Cleanup] Further bot.cpp nullptr checks
    # Notes
    - `nullptr` validation

--- zone/bot.cpp
+++ zone/bot.cpp
@@ -3140,7 +3140,6 @@ void Bot::SetOwnerTarget(Client* bot_owner) {
 			SetTarget(attack_target);
 			SetAttackingFlag();
 			if (HasPet() && (GetClass() != ENCHANTER || GetPet()->GetPetType() != petAnimation || GetAA(aaAnimationEmpathy) >= 2)) {
-
 				GetPet()->WipeHateList();
 				GetPet()->AddToHateList(attack_target, 1);
 				GetPet()->SetTarget(attack_target);
@@ -4394,6 +4393,10 @@ void Bot::PerformTradeWithClient(int16 begin_slot_id, int16 end_slot_id, Client*
 	// trades can now go in as empty slot inserts
 	for (auto& trade_iterator : client_trade) {
 		// TODO: code for stackables
+
+		if (!trade_iterator.trade_item_instance) {
+			continue;
+		}
 
 		if (!database.botdb.SaveItemBySlot(this, trade_iterator.to_bot_slot, trade_iterator.trade_item_instance)) {
 			OwnerMessage(
