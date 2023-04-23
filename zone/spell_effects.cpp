6452adde9be0c73d2dbdd4c43417f43cdb89e3a7
Trust <fryguy503@gmail.com>
2023-04-23 16:51:07 -0400

    {EQEmu PR 3297} [Cleanup] Breaks in wrong spot in cases in spell_effects.cpp
    # Notes
    - These breaks were inside conditions, meaning the case wasn't always broken.

--- zone/spell_effects.cpp
+++ zone/spell_effects.cpp
@@ -1480,6 +1480,7 @@ bool Mob::SpellEffect(Mob* caster, uint16 spell_id, float partial, int level_ove
 						for (int x = EQ::textures::textureBegin; x <= EQ::textures::LastTintableTexture; x++)
 							caster->SendWearChange(x);
 				}
+				break;
 			}
 
 			case SE_WipeHateList:
@@ -2999,9 +3000,9 @@ bool Mob::SpellEffect(Mob* caster, uint16 spell_id, float partial, int level_ove
 						int shield_target_mitigation = spells[spell_id].limit_value[i] ? spells[spell_id].limit_value[i] : 50;
 						int shielder_mitigation      = spells[spell_id].max_value[i] ? spells[spell_id].limit_value[i] : 50;
 						ShieldAbility(petowner->GetID(), 25, shield_duration, shield_target_mitigation, shielder_mitigation);
-						break;
 					}
 				}
+				break;
 			}
 
 			case SE_Weapon_Stance: {
@@ -4466,9 +4467,9 @@ void Mob::BuffFadeBySlot(int slot, bool iRecalcBonuses)
 					if (flee_mode) {
 						currently_fleeing = true;
 						CheckFlee();
-						break;
 					}
 				}
+				break;
 			}
 
 			case SE_BindSight:
@@ -4507,6 +4508,7 @@ void Mob::BuffFadeBySlot(int slot, bool iRecalcBonuses)
 					}
 					CastToClient()->SetControlledMobId(0);
 				}
+				break;
 			}
 
 			case SE_Weapon_Stance:
@@ -5171,9 +5173,9 @@ int64 Mob::CalcAAFocus(focusType type, const AA::Rank &rank, uint16 spell_id)
 						value        = 0;
 						LimitFailure = true;
 					}
-					break;
 				}
-
+				break;
+				
 			case SE_FcSpellVulnerability:
 				if (type == focusSpellVulnerability) {
 					value = base_value;
