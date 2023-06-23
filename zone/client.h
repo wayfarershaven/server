d3bcbf395132f236e52a81540e32abec5b7dda3d
Trust <fryguy503@gmail.com>
2023-06-23 13:26:02 -0400

    {EQEmu PR 3429} [Commands] Add #showspells Command
    # Notes
    - Add `#showspells` command to show your or your target's memorized spells or learned disciplines.
    
    # Disciplines
    ![image](https://github.com/EQEmu/Server/assets/89047260/8ffe3518-f862-4354-84ac-c4b6da618037)
    
    # Spells
    ![image](https://github.com/EQEmu/Server/assets/89047260/552a2a17-b0c9-4e8a-a414-8e7900712581)

--- zone/client.h
+++ zone/client.h
@@ -133,6 +133,12 @@ enum {
 
 typedef enum
 {
+	Disciplines,
+	Spells
+} ShowSpellType;
+
+typedef enum
+{
 	Empty = 0,
 	Auto = 1,
 	CurrentTargetPC = 2,
@@ -640,6 +646,8 @@ public:
 	inline const int32 GetInstanceID() const { return zone->GetInstanceID(); }
 	void SetZoning(bool in) { bZoning = in; }
 
+	void ShowSpells(Client* c, ShowSpellType show_spell_type);
+	
 	FACTION_VALUE GetReverseFactionCon(Mob* iOther);
 	FACTION_VALUE GetFactionLevel(uint32 char_id, uint32 npc_id, uint32 p_race, uint32 p_class, uint32 p_deity, int32 pFaction, Mob* tnpc);
 	bool ReloadCharacterFaction(Client *c, uint32 facid, uint32 charid);
