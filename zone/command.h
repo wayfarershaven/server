b3824c97b395ed203a88b21e52a9a850b8d114bb
Trust <fryguy503@gmail.com>
2023-06-23 13:28:21 -0400

    {EQEmu PR 3431} [Cleanup] Remove command_unlock from zone/command.h
    # Notes
    - This is unused.

--- zone/command.h
+++ zone/command.h
@@ -286,7 +286,6 @@ void command_tune(Client *c, const Seperator *sep);
 void command_undeletechar(Client *c, const Seperator *sep);
 void command_undye(Client *c, const Seperator *sep);
 void command_unfreeze(Client *c, const Seperator *sep);
-void command_unlock(Client *c, const Seperator *sep);
 void command_unmemspell(Client *c, const Seperator *sep);
 void command_unmemspells(Client *c, const Seperator *sep);
 void command_unscribespell(Client *c, const Seperator *sep);
