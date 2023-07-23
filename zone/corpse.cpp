8b724c77640ffc6d9645ed3eefba4ec3634df907
Trust <fryguy503@gmail.com>
2023-07-23 17:20:52 -0400

    {EQEmu PR 3511} [Cleanup] Fix casing in corpse money.
    # Notes
    - These were uppercase and should be lowercase.

--- zone/corpse.cpp
+++ zone/corpse.cpp
@@ -1195,7 +1195,7 @@ void Corpse::MakeLootRequestPackets(Client* client, const EQApplicationPacket* a
 			client->Message(
 				Chat::Yellow,
 				fmt::format(
-					"This corpse Contains {}.",
+					"This corpse contains {}.",
 					Strings::Money(
 						GetPlatinum(),
 						GetGold(),
@@ -1205,7 +1205,7 @@ void Corpse::MakeLootRequestPackets(Client* client, const EQApplicationPacket* a
 				).c_str()
 			);
 		} else {
-			client->Message(Chat::Yellow, "This corpse Contains no money.");
+			client->Message(Chat::Yellow, "This corpse contains no money.");
 		}
 
 		auto outapp = new EQApplicationPacket(OP_MoneyOnCorpse, sizeof(moneyOnCorpseStruct));
