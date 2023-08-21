wayfarershaven/server

commit 3f3bbe98b51eee1a5ba4a0b5deda4c38219b8c86

[Data Buckets] Implement scoped data buckets (#3498)
* [Data Buckets] Implement scoped data buckets

* Update database_update_manifest.cpp

* Update data_bucket.cpp

* Update data_bucket.cpp

* Update data_bucket.cpp

* Update data_bucket.h

* Update database_update_manifest.cpp

* Add GetScopedDbFilters references

* Scope transfer

zone/bot.cpp
@@ -8193,21 +8193,16 @@ void Bot::OwnerMessage(const std::string& message)
 bool Bot::CheckDataBucket(std::string bucket_name, const std::string& bucket_value, uint8 bucket_comparison)
 {
 	if (!bucket_name.empty() && !bucket_value.empty()) {
-		auto full_name = fmt::format(
-			"{}-{}",
-			GetBucketKey(),
-			bucket_name
-		);
+		// try to fetch from bot first
+		DataBucketKey k = GetScopedBucketKeys();
+		k.key = bucket_name;
 
-		auto player_value = DataBucket::CheckBucketKey(this, full_name);
+		auto player_value = DataBucket::CheckBucketKey(this, k);
 		if (player_value.empty() && GetBotOwner()) {
-			full_name = fmt::format(
-				"{}-{}",
-				GetBotOwner()->GetBucketKey(),
-				bucket_name
-			);
+			// fetch from owner
+			k = GetBotOwner()->GetScopedBucketKeys();
 
-			player_value = DataBucket::CheckBucketKey(GetBotOwner(), full_name);
+			player_value = DataBucket::CheckBucketKey(GetBotOwner(), k);
 			if (player_value.empty()) {
 				return false;
 			}

common/database/database_update_manifest.cpp
@@ -4806,6 +4806,27 @@ UNIQUE INDEX `command`(`parent_command`, `sub_command`)
 )
 )"
 	},
+	ManifestEntry{
+		.version = 9233,
+		.description = "2023_07_16_scoped_data_buckets.sql",
+		.check = "SHOW COLUMNS FROM `data_buckets` LIKE 'character_id'",
+		.condition = "empty",
+		.match = "",
+		.sql = R"(
+
+ALTER TABLE `data_buckets`
+ADD COLUMN `character_id` bigint(11) NOT NULL DEFAULT 0 AFTER `expires`,
+ADD COLUMN `npc_id` bigint(11) NOT NULL DEFAULT 0 AFTER `character_id`,
+ADD COLUMN `bot_id` bigint(11) NOT NULL DEFAULT 0 AFTER `npc_id`,
+DROP INDEX `key_index`,
+ADD UNIQUE INDEX `keys`(`key`,`character_id`,`npc_id`,`bot_id`);
+
+UPDATE data_buckets SET character_id = SUBSTRING_INDEX(SUBSTRING_INDEX( `key`, '-', 2 ), '-', -1), `key` = SUBSTR(SUBSTRING_INDEX(`key`, SUBSTRING_INDEX( `key`, '-', 2 ), -1), 2) WHERE `key` LIKE 'character-%';
+UPDATE data_buckets SET npc_id = SUBSTRING_INDEX(SUBSTRING_INDEX( `key`, '-', 2 ), '-', -1), `key` = SUBSTR(SUBSTRING_INDEX(`key`, SUBSTRING_INDEX( `key`, '-', 2 ), -1), 2) WHERE `key` LIKE 'npc-%';
+UPDATE data_buckets SET bot_id = SUBSTRING_INDEX(SUBSTRING_INDEX( `key`, '-', 2 ), '-', -1), `key` = SUBSTR(SUBSTRING_INDEX(`key`, SUBSTRING_INDEX( `key`, '-', 2 ), -1), 2) WHERE `key` LIKE 'bot-%';
+
+)"
+	},
 
 // -- template; copy/paste this when you need to create a new entry
 //	ManifestEntry{

common/repositories/base/base_data_buckets_repository.h
@@ -24,6 +24,9 @@ public:
 		std::string key_;
 		std::string value;
 		uint32_t    expires;
+		int64_t     character_id;
+		int64_t     npc_id;
+		int64_t     bot_id;
 	};
 
 	static std::string PrimaryKey()
@@ -38,6 +41,9 @@ public:
 			"`key`",
 			"value",
 			"expires",
+			"character_id",
+			"npc_id",
+			"bot_id",
 		};
 	}
 
@@ -48,6 +54,9 @@ public:
 			"`key`",
 			"value",
 			"expires",
+			"character_id",
+			"npc_id",
+			"bot_id",
 		};
 	}
 
@@ -88,10 +97,13 @@ public:
 	{
 		DataBuckets e{};
 
-		e.id      = 0;
-		e.key_    = "";
-		e.value   = "";
-		e.expires = 0;
+		e.id           = 0;
+		e.key_         = "";
+		e.value        = "";
+		e.expires      = 0;
+		e.character_id = 0;
+		e.npc_id       = 0;
+		e.bot_id       = 0;
 
 		return e;
 	}
@@ -128,10 +140,13 @@ public:
 		if (results.RowCount() == 1) {
 			DataBuckets e{};
 
-			e.id      = strtoull(row[0], nullptr, 10);
-			e.key_    = row[1] ? row[1] : "";
-			e.value   = row[2] ? row[2] : "";
-			e.expires = static_cast<uint32_t>(strtoul(row[3], nullptr, 10));
+			e.id           = strtoull(row[0], nullptr, 10);
+			e.key_         = row[1] ? row[1] : "";
+			e.value        = row[2] ? row[2] : "";
+			e.expires      = static_cast<uint32_t>(strtoul(row[3], nullptr, 10));
+			e.character_id = strtoll(row[4], nullptr, 10);
+			e.npc_id       = strtoll(row[5], nullptr, 10);
+			e.bot_id       = strtoll(row[6], nullptr, 10);
 
 			return e;
 		}
@@ -168,6 +183,9 @@ public:
 		v.push_back(columns[1] + " = '" + Strings::Escape(e.key_) + "'");
 		v.push_back(columns[2] + " = '" + Strings::Escape(e.value) + "'");
 		v.push_back(columns[3] + " = " + std::to_string(e.expires));
+		v.push_back(columns[4] + " = " + std::to_string(e.character_id));
+		v.push_back(columns[5] + " = " + std::to_string(e.npc_id));
+		v.push_back(columns[6] + " = " + std::to_string(e.bot_id));
 
 		auto results = db.QueryDatabase(
 			fmt::format(
@@ -193,6 +211,9 @@ public:
 		v.push_back("'" + Strings::Escape(e.key_) + "'");
 		v.push_back("'" + Strings::Escape(e.value) + "'");
 		v.push_back(std::to_string(e.expires));
+		v.push_back(std::to_string(e.character_id));
+		v.push_back(std::to_string(e.npc_id));
+		v.push_back(std::to_string(e.bot_id));
 
 		auto results = db.QueryDatabase(
 			fmt::format(
@@ -226,6 +247,9 @@ public:
 			v.push_back("'" + Strings::Escape(e.key_) + "'");
 			v.push_back("'" + Strings::Escape(e.value) + "'");
 			v.push_back(std::to_string(e.expires));
+			v.push_back(std::to_string(e.character_id));
+			v.push_back(std::to_string(e.npc_id));
+			v.push_back(std::to_string(e.bot_id));
 
 			insert_chunks.push_back("(" + Strings::Implode(",", v) + ")");
 		}
@@ -259,10 +283,13 @@ public:
 		for (auto row = results.begin(); row != results.end(); ++row) {
 			DataBuckets e{};
 
-			e.id      = strtoull(row[0], nullptr, 10);
-			e.key_    = row[1] ? row[1] : "";
-			e.value   = row[2] ? row[2] : "";
-			e.expires = static_cast<uint32_t>(strtoul(row[3], nullptr, 10));
+			e.id           = strtoull(row[0], nullptr, 10);
+			e.key_         = row[1] ? row[1] : "";
+			e.value        = row[2] ? row[2] : "";
+			e.expires      = static_cast<uint32_t>(strtoul(row[3], nullptr, 10));
+			e.character_id = strtoll(row[4], nullptr, 10);
+			e.npc_id       = strtoll(row[5], nullptr, 10);
+			e.bot_id       = strtoll(row[6], nullptr, 10);
 
 			all_entries.push_back(e);
 		}
@@ -287,10 +314,13 @@ public:
 		for (auto row = results.begin(); row != results.end(); ++row) {
 			DataBuckets e{};
 
-			e.id      = strtoull(row[0], nullptr, 10);
-			e.key_    = row[1] ? row[1] : "";
-			e.value   = row[2] ? row[2] : "";
-			e.expires = static_cast<uint32_t>(strtoul(row[3], nullptr, 10));
+			e.id           = strtoull(row[0], nullptr, 10);
+			e.key_         = row[1] ? row[1] : "";
+			e.value        = row[2] ? row[2] : "";
+			e.expires      = static_cast<uint32_t>(strtoul(row[3], nullptr, 10));
+			e.character_id = strtoll(row[4], nullptr, 10);
+			e.npc_id       = strtoll(row[5], nullptr, 10);
+			e.bot_id       = strtoll(row[6], nullptr, 10);
 
 			all_entries.push_back(e);
 		}

common/version.h
@@ -42,7 +42,7 @@
  * Manifest: https://github.com/EQEmu/Server/blob/master/utils/sql/db_update_manifest.txt
  */
 
-#define CURRENT_BINARY_DATABASE_VERSION 9232
+#define CURRENT_BINARY_DATABASE_VERSION 9233
 
 #define CURRENT_BINARY_BOTS_DATABASE_VERSION 9039
 

zone/bonuses.cpp
@@ -6033,14 +6033,10 @@ float Mob::CheckHeroicBonusesDataBuckets(std::string bucket_name)
 {
 	std::string bucket_value;
 	if (!bucket_name.empty()) {
-		const auto full_name = fmt::format(
-			"{}-{}",
-			GetBucketKey(),
-			bucket_name
-		);
-
+		DataBucketKey k = GetScopedBucketKeys();
+		k.key = bucket_name;
 		if (IsOfClientBot()) {
-			bucket_value = DataBucket::CheckBucketKey(this, full_name);
+			bucket_value = DataBucket::CheckBucketKey(this, k);
 		}
 
 		if (bucket_value.empty() || !Strings::IsNumber(bucket_value)) {

zone/client_process.cpp
@@ -853,13 +853,11 @@ void Client::BulkSendMerchantInventory(int merchant_id, int npcid) {
 		auto bucket_name = ml.bucket_name;
 		auto const& bucket_value = ml.bucket_value;
 		if (!bucket_name.empty() && !bucket_value.empty()) {
-			auto full_name = fmt::format(
-				"{}-{}",
-				GetBucketKey(),
-				bucket_name
-			);
 
-			auto const& player_value = DataBucket::CheckBucketKey(this, full_name);
+			DataBucketKey k = GetScopedBucketKeys();
+			k.key = bucket_name;
+
+			auto const& player_value = DataBucket::CheckBucketKey(this, k);
 			if (player_value.empty()) {
 				continue;
 			}

zone/data_bucket.cpp
@@ -1,173 +1,130 @@
 #include "data_bucket.h"
 #include "zonedb.h"
+#include "mob.h"
 #include <ctime>
 #include <cctype>
+#include "../common/repositories/data_buckets_repository.h"
 
-/**
- * Persists data via bucket_name as key
- * @param bucket_key
- * @param bucket_value
- * @param expires_time
- */
-void DataBucket::SetData(const std::string& bucket_key, const std::string& bucket_value, std::string expires_time) {
-	uint64 bucket_id = DataBucket::DoesBucketExist(bucket_key);
+void DataBucket::SetData(const std::string &bucket_key, const std::string &bucket_value, std::string expires_time)
+{
+	auto k = DataBucketKey{
+		.key = bucket_key,
+		.value = bucket_value,
+		.expires = expires_time,
+		.character_id = 0,
+		.npc_id = 0,
+		.bot_id = 0
+	};
 
-	std::string query;
+	DataBucket::SetData(k);
+}
+
+void DataBucket::SetData(const DataBucketKey &k)
+{
+	auto r = DataBucketsRepository::GetWhere(
+		database,
+		fmt::format(
+			"{} `key` = '{}' AND (`expires` > {} OR `expires` = 0) LIMIT 1",
+			DataBucket::GetScopedDbFilters(k),
+			Strings::Escape(k.key),
+			(long long) std::time(nullptr)
+		)
+	);
+
+	// if we have an entry, use it
+	auto b = DataBucketsRepository::NewEntity();
+	if (!r.empty()) {
+		b = r[0];
+	}
+
+	if (k.character_id > 0) {
+		b.character_id = k.character_id;
+	} else if (k.npc_id > 0) {
+		b.npc_id = k.npc_id;
+	} else if (k.bot_id > 0) {
+		b.bot_id = k.bot_id;
+	}
+
+	uint64    bucket_id         = b.id;
 	long long expires_time_unix = 0;
 
-	if (!expires_time.empty()) {
-		if (isalpha(expires_time[0]) || isalpha(expires_time[expires_time.length() - 1])) {
-			expires_time_unix = (long long) std::time(nullptr) + Strings::TimeToSeconds(expires_time);
-		} else {
-			expires_time_unix = (long long) std::time(nullptr) + Strings::ToInt(expires_time);
+	if (!k.expires.empty()) {
+		expires_time_unix = (long long) std::time(nullptr) + Strings::ToInt(k.expires);
+		if (isalpha(k.expires[0]) || isalpha(k.expires[k.expires.length() - 1])) {
+			expires_time_unix = (long long) std::time(nullptr) + Strings::TimeToSeconds(k.expires);
 		}
 	}
 
 	if (bucket_id > 0) {
-		std::string update_expired_time;
-		if (expires_time_unix > 0) {
-			update_expired_time = StringFormat(", `expires` = %lld ", expires_time_unix);
-		}
-
-		query = StringFormat(
-				"UPDATE `data_buckets` SET `value` = '%s' %s WHERE `id` = %i",
-				Strings::Escape(bucket_value).c_str(),
-				Strings::Escape(update_expired_time).c_str(),
-				bucket_id
-		);
+		b.expires = expires_time_unix;
+		b.value   = k.value;
+		DataBucketsRepository::UpdateOne(database, b);
 	}
 	else {
-		query = StringFormat(
-				"INSERT INTO `data_buckets` (`key`, `value`, `expires`) VALUES ('%s', '%s', '%lld')",
-				Strings::Escape(bucket_key).c_str(),
-				Strings::Escape(bucket_value).c_str(),
-				expires_time_unix
-		);
+		b.expires = expires_time_unix;
+		b.key_    = k.key;
+		b.value   = k.value;
+		DataBucketsRepository::InsertOne(database, b);
 	}
-
-	database.QueryDatabase(query);
 }
 
-/**
- * Retrieves data via bucket_name as key
- * @param bucket_key
- * @return
- */
-std::string DataBucket::GetData(const std::string& bucket_key) {
-	std::string query = StringFormat(
-			"SELECT `value` from `data_buckets` WHERE `key` = '%s' AND (`expires` > %lld OR `expires` = 0)  LIMIT 1",
-			bucket_key.c_str(),
-			(long long) std::time(nullptr)
-	);
-
-	auto results = database.QueryDatabase(query);
-	if (!results.Success()) {
-		return std::string();
-	}
-
-	if (results.RowCount() != 1)
-		return std::string();
-
-	auto row = results.begin();
-
-	return std::string(row[0]);
-}
-
-/**
- * Retrieves data expires time via bucket_name as key
- * @param bucket_key
- * @return
- */
-std::string DataBucket::GetDataExpires(const std::string& bucket_key) {
-	std::string query = StringFormat(
-			"SELECT `expires` from `data_buckets` WHERE `key` = '%s' AND (`expires` > %lld OR `expires` = 0)  LIMIT 1",
-			bucket_key.c_str(),
-			(long long) std::time(nullptr)
-	);
-
-	auto results = database.QueryDatabase(query);
-	if (!results.Success()) {
-		return std::string();
-	}
-
-	if (results.RowCount() != 1)
-		return std::string();
-
-	auto row = results.begin();
-
-	return std::string(row[0]);
-}
-
-std::string DataBucket::GetDataRemaining(const std::string& bucket_key) {
-	if (DataBucket::GetDataExpires(bucket_key).empty()) {
-		return "0";
-	}
-	std::string query = fmt::format(
-		"SELECT (`expires` - UNIX_TIMESTAMP()) AS `remaining` from `data_buckets` WHERE `key` = '{}' AND (`expires` > {} OR `expires` = 0) LIMIT 1",
-		bucket_key.c_str(),
-		(long long) std::time(nullptr)
-	);
-
-	auto results = database.QueryDatabase(query);
-	if (!results.Success()) {
-		return std::string();
-	}
-
-	if (results.RowCount() != 1)
-		return std::string();
-
-	auto row = results.begin();
-
-	return std::string(row[0]);
-}
-
-/**
- * Checks for bucket existence by bucket_name key
- * @param bucket_key
- * @return
- */
-uint64 DataBucket::DoesBucketExist(const std::string& bucket_key) {
-	std::string query = StringFormat(
-			"SELECT `id` from `data_buckets` WHERE `key` = '%s' AND (`expires` > %lld OR `expires` = 0) LIMIT 1",
-			Strings::Escape(bucket_key).c_str(),
-			(long long) std::time(nullptr)
-	);
-
-	auto results = database.QueryDatabase(query);
-	if (!results.Success()) {
-		return 0;
-	}
-
-	auto row = results.begin();
-	if (results.RowCount() != 1)
-		return 0;
-
-	return Strings::ToUnsignedBigInt(row[0]);
-}
-
-/**
- * Deletes data bucket by key
- * @param bucket_key
- * @return
- */
-bool DataBucket::DeleteData(const std::string& bucket_key) {
-	std::string query = StringFormat(
-			"DELETE FROM `data_buckets` WHERE `key` = '%s'",
-			Strings::Escape(bucket_key).c_str()
-	);
-
-	auto results = database.QueryDatabase(query);
-
-	return results.Success();
-}
-
-bool DataBucket::GetDataBuckets(Mob* mob)
+std::string DataBucket::GetData(const std::string &bucket_key)
 {
-	auto l = BaseDataBucketsRepository::GetWhere(
+	DataBucketKey k = {};
+	k.key = bucket_key;
+	return GetData(k);
+}
+
+std::string DataBucket::GetData(const DataBucketKey &k)
+{
+	auto r = DataBucketsRepository::GetWhere(
 		database,
 		fmt::format(
-			"`key` LIKE '{}-%'",
-			Strings::Escape(mob->GetBucketKey())
+			"{} `key` = '{}' AND (`expires` > {} OR `expires` = 0) LIMIT 1",
+			DataBucket::GetScopedDbFilters(k),
+			k.key,
+			(long long) std::time(nullptr)
+		)
+	);
+
+	if (r.empty()) {
+		return {};
+	}
+
+	return r[0].value;
+}
+
+std::string DataBucket::GetDataExpires(const std::string &bucket_key)
+{
+	DataBucketKey k = {};
+	k.key = bucket_key;
+
+	return GetDataExpires(k);
+}
+
+std::string DataBucket::GetDataRemaining(const std::string &bucket_key)
+{
+	DataBucketKey k = {};
+	k.key = bucket_key;
+	return GetDataRemaining(k);
+}
+
+bool DataBucket::DeleteData(const std::string &bucket_key)
+{
+	DataBucketKey r = {};
+	r.key = bucket_key;
+	return DeleteData(r);
+}
+
+bool DataBucket::GetDataBuckets(Mob *mob)
+{
+	DataBucketKey k = mob->GetScopedBucketKeys();
+	auto          l = BaseDataBucketsRepository::GetWhere(
+		database,
+		fmt::format(
+			"{} (`expires` > {} OR `expires` = 0)",
+			DataBucket::GetScopedDbFilters(k),
+			(long long) std::time(nullptr)
 		)
 	);
 
@@ -179,10 +136,10 @@ bool DataBucket::GetDataBuckets(Mob* mob)
 
 	DataBucketCache d;
 
-	for (const auto& e : l) {
-		d.bucket_id = e.id;
-		d.bucket_key = e.key_;
-		d.bucket_value = e.value;
+	for (const auto &e: l) {
+		d.bucket_id      = e.id;
+		d.bucket_key     = e.key_;
+		d.bucket_value   = e.value;
 		d.bucket_expires = e.expires;
 
 		mob->m_data_bucket_cache.emplace_back(d);
@@ -191,11 +148,11 @@ bool DataBucket::GetDataBuckets(Mob* mob)
 	return true;
 }
 
-std::string DataBucket::CheckBucketKey(const Mob* mob, std::string_view full_name)
+std::string DataBucket::CheckBucketKey(const Mob *mob, const DataBucketKey &k)
 {
-	std::string bucket_value;
-	for (const auto &d : mob->m_data_bucket_cache) {
-		if (d.bucket_key == full_name) {
+	std::string     bucket_value;
+	for (const auto &d: mob->m_data_bucket_cache) {
+		if (d.bucket_key == k.key) {
 			bucket_value = d.bucket_value;
 			break;
 		}
@@ -203,3 +160,72 @@ std::string DataBucket::CheckBucketKey(const Mob* mob, std::string_view full_nam
 	return bucket_value;
 }
 
+bool DataBucket::DeleteData(const DataBucketKey &k)
+{
+	return DataBucketsRepository::DeleteWhere(
+		database,
+		fmt::format(
+			"{} `key` = '{}'",
+			DataBucket::GetScopedDbFilters(k),
+			k.key
+		)
+	);
+}
+
+std::string DataBucket::GetDataExpires(const DataBucketKey &k)
+{
+	auto r = DataBucketsRepository::GetWhere(
+		database,
+		fmt::format(
+			"{} `key` = '{}' AND (`expires` > {} OR `expires` = 0) LIMIT 1",
+			DataBucket::GetScopedDbFilters(k),
+			k.key,
+			(long long) std::time(nullptr)
+		)
+	);
+
+	if (r.empty()) {
+		return {};
+	}
+
+	return fmt::format("{}", r[0].expires);
+}
+
+std::string DataBucket::GetDataRemaining(const DataBucketKey &k)
+{
+	auto r = DataBucketsRepository::GetWhere(
+		database,
+		fmt::format(
+			"{} `key` = '{}' AND (`expires` > {} OR `expires` = 0) LIMIT 1",
+			DataBucket::GetScopedDbFilters(k),
+			k.key,
+			(long long) std::time(nullptr)
+		)
+	);
+
+	if (r.empty()) {
+		return "0";
+	}
+
+	return fmt::format("{}", r[0].expires - (long long) std::time(nullptr));
+}
+
+std::string DataBucket::GetScopedDbFilters(const DataBucketKey &k)
+{
+	std::vector<std::string> query = {};
+	if (k.character_id > 0) {
+		query.emplace_back(fmt::format("character_id = {}", k.character_id));
+	}
+	else if (k.npc_id > 0) {
+		query.emplace_back(fmt::format("npc_id = {}", k.npc_id));
+	}
+	else if (k.bot_id > 0) {
+		query.emplace_back(fmt::format("bot_id = {}", k.bot_id));
+	}
+
+	return fmt::format(
+		"{} {}",
+		Strings::Join(query, " AND "),
+		!query.empty() ? "AND" : ""
+	);
+}

zone/data_bucket.h
@@ -10,18 +10,35 @@
 #include "../common/repositories/data_buckets_repository.h"
 #include "mob.h"
 
+
+struct DataBucketKey {
+	std::string key;
+	std::string value;
+	std::string expires;
+	int64_t     character_id;
+	int64_t     npc_id;
+	int64_t     bot_id;
+};
+
 class DataBucket {
 public:
+	// non-scoped bucket methods (for global buckets)
 	static void SetData(const std::string& bucket_key, const std::string& bucket_value, std::string expires_time = "");
 	static bool DeleteData(const std::string& bucket_key);
 	static std::string GetData(const std::string& bucket_key);
 	static std::string GetDataExpires(const std::string& bucket_key);
 	static std::string GetDataRemaining(const std::string& bucket_key);
-	static bool GetDataBuckets(Mob* mob);
-	static std::string CheckBucketKey(const Mob* mob, std::string_view full_name);
 
-private:
-	static uint64 DoesBucketExist(const std::string& bucket_key);
+	static bool GetDataBuckets(Mob* mob);
+
+	// scoped bucket methods
+	static void SetData(const DataBucketKey& k);
+	static bool DeleteData(const DataBucketKey& k);
+	static std::string GetData(const DataBucketKey& k);
+	static std::string GetDataExpires(const DataBucketKey& k);
+	static std::string GetDataRemaining(const DataBucketKey& k);
+	static std::string CheckBucketKey(const Mob* mob, const DataBucketKey& k);
+	static std::string GetScopedDbFilters(const DataBucketKey& k);
 };
 
 #endif //EQEMU_DATABUCKET_H

commit ead55c943ad4e265efb4ef83978c0f15b62cfe81

{EQEmu PR 3552} [Bug Fix] Fix Bot::CheckDataBucket to work with Owner Buckets.
Fixed regression from #3498, Bot::CheckDataBucket was not properly checking owner buckets as ```k.key``` was not being set after ```k = GetBotOwner()->GetScopedBucketKeys();```.

zone/bot.cpp
@@ -8253,6 +8253,7 @@ bool Bot::CheckDataBucket(std::string bucket_name, const std::string& bucket_val
 		if (b.value.empty() && GetBotOwner()) {
 			// fetch from owner
 			k = GetBotOwner()->GetScopedBucketKeys();
+			k.key = bucket_name;
 
 			b = DataBucket::GetData(k);
 			if (b.value.empty()) {