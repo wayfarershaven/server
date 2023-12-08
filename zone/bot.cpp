d78519831d7b3c6cdb74826e0fe6c280c54480b6
Trust <fryguy503@gmail.com>
2023-12-08 09:04:35 -0500

    {EQEmu PR 3744} [Bots] Fix bots learning spells on level
    Previously bots would need to be zoned or camped/spawned to begin using new spells for their level.

--- zone/bot.cpp
+++ zone/bot.cpp
@@ -3637,6 +3637,7 @@ void Bot::LevelBotWithClient(Client* client, uint8 level, bool sendlvlapp) {
 					bot->SendLevelAppearance();
 				// modified from Client::SetLevel()
 				bot->SendAppearancePacket(AT_WhoLevel, level, true, true); // who level change
+				bot->AI_AddBotSpells(bot->GetBotSpellID());
 			}
 		}
 
