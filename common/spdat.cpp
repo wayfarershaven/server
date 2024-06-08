fde3193bc05be4b2eb54d6def7454f7713205dd6
Trust <fryguy503@gmail.com>
2024-06-07 22:45:44 -0500

    [Bug] Prevent Ressurection Spells from being resisted
    Added IsRessurectionSpell(uint16 spell_id) to assist with checking on a spell is a ressurection spell.

--- common/spdat.cpp
+++ common/spdat.cpp
@@ -431,6 +431,16 @@ bool IsCharmSpell(uint16 spell_id)
 	return IsEffectInSpell(spell_id, SE_Charm);
 }
 
+bool IsRessurectionSpell(uint16 spell_id) {
+	return (
+		spell_id == SPELL_RESURRECTION_SICKNESS ||
+		spell_id == SPELL_RESURRECTION_SICKNESS2 ||
+		spell_id == SPELL_RESURRECTION_SICKNESS3 ||
+		spell_id == SPELL_RESURRECTION_SICKNESS4 ||
+		spell_id == SPELL_REVIVAL_SICKNESS
+	);
+}
+
 bool IsBlindSpell(uint16 spell_id)
 {
 	return IsEffectInSpell(spell_id, SE_Blind);
@@ -1449,6 +1459,7 @@ bool IsDebuffSpell(uint16 spell_id)
 		IsStunSpell(spell_id) ||
 		IsMesmerizeSpell(spell_id) ||
 		IsCharmSpell(spell_id) ||
+		IsRessurectionSpell(spell_id) ||
 		IsSlowSpell(spell_id) ||
 		IsEffectInSpell(spell_id, SE_Root) ||
 		IsEffectInSpell(spell_id, SE_CancelMagic) ||
