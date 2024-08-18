1e8843ec12d287325be292e571157484c9d3b63d
Trust <fryguy503@gmail.com>
2024-08-17 23:03:37 -0500

    Try this logic.

--- zone/exp.cpp
+++ zone/exp.cpp
@@ -793,8 +793,10 @@ void Client::SetEXP(ExpSource exp_source, uint64 set_exp, uint64 set_aaxp, bool 
 	auto client_max_level = GetClientMaxLevel();
 	if (client_max_level) {
 		if (GetLevel() >= client_max_level) {
-			auto exp_needed = GetEXPForLevel(client_max_level + 1);
-			exp_needed - 100;
+			auto level_above_exp_needed = GetEXPForLevel(client_max_level + 1);
+			auto max_level_exp_needed = GetEXPForLevel(client_max_level);
+			auto exp_needed = max_level_exp_needed + (level_above_exp_needed - max_level_exp_needed - 1000);
+
 			if (set_exp > exp_needed) {
 				set_exp = exp_needed;
 			}
