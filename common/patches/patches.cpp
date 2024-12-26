6ba8b26f6fd00aeb7851e05671c5b75028b8d759
Trust <fryguy503@gmail.com>
2024-12-25 23:20:00 -0600

    Disable all other clients.

--- common/patches/patches.cpp
+++ common/patches/patches.cpp
@@ -11,7 +11,7 @@
 	are required to give you total support for your newly bought product;
 	without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 	A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-	
+
 	You should have received a copy of the GNU General Public License
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -30,20 +30,20 @@
 
 void RegisterAllPatches(EQStreamIdentifier &into)
 {
-	Titanium::Register(into);
-	SoF::Register(into);
-	SoD::Register(into);
-	UF::Register(into);
-	RoF::Register(into);
+//	Titanium::Register(into);
+//	SoF::Register(into);
+//	SoD::Register(into);
+//	UF::Register(into);
+//	RoF::Register(into);
 	RoF2::Register(into);
 }
 
 void ReloadAllPatches()
 {
-	Titanium::Reload();
-	SoF::Reload();
-	SoD::Reload();
-	UF::Reload();
-	RoF::Reload();
+//	Titanium::Reload();
+//	SoF::Reload();
+//	SoD::Reload();
+//	UF::Reload();
+//	RoF::Reload();
 	RoF2::Reload();
 }
