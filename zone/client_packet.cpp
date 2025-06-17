1bc4c4f986697a8075a923105661830c75b84837
Trust <fryguy503@gmail.com>
2025-06-17 13:17:01 -0400

    Revert "Add Logging"
    This reverts commit 7712eeb3f4acc89bd1f1cabd5306f06b0c84ee0f.

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -4911,8 +4911,6 @@ void Client::Handle_OP_CAuth(const EQApplicationPacket *app) {
 		LogDebug("Got HAX VALUE from [{}]", GetCleanName());
 	}
 
-	LogDebug("decryptedValue = [{}] - GetPlayerClassBit = [{}] - GetID = [{}]", decryptedValue, GetPlayerClassBit(m_pp.class_), GetID());
-
 	CAuthorized = (decryptedValue == (GetPlayerClassBit(m_pp.class_) * GetID()));
 
 	if (GetGM()) {
